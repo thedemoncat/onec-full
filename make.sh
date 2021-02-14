c587d2323b2400ae12631a4943d29f602720ecdb
ZhdanovR <nl.ruslan@yandex.ru>
2021-02-14 21:40:27 +0600

    добавил скрипт прописывающий поды с серверами 1С

--- make.sh
+++ make.sh
@@ -3,8 +3,10 @@ set -e
 
 export $(grep -v '^#' .env | xargs)
 
-docker build -t demoncat/onec-full:"$ONEC_VERSION" \
+docker build -t demoncat/onec:full-"$ONEC_VERSION" \
     --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
     --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
     --build-arg VERSION="$ONEC_VERSION" .
+
+docker build -t demoncat/onec:full-"$ONEC_VERSION"-k8s --build-arg VERSION="$ONEC_VERSION" -f Dockerfile_k8s .
  
