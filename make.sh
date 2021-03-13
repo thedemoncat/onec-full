55dcacccbf513d98fac94e0e3a0831cf66ccafb8
Ruslan Zhdanov <26283990+TheDemonCat@users.noreply.github.com>
2021-03-13 23:12:06 +0600

    Сборка образа (#3)
    * Тестирование сборки образа

--- make.sh
+++ make.sh
@@ -5,13 +5,16 @@ if [ -f ".env" ]; then
 fi
 
 buildImages() {
+
+    
+
     _ONEC_VERSION=$1
-    docker build -t demoncat/onec:full-"$_ONEC_VERSION" \
+    docker build -t thedemoncat/onec/full:"$_ONEC_VERSION" \
         --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
         --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
         --build-arg ONEC_VERSION="$_ONEC_VERSION" .
 
-    docker build -t demoncat/onec:full-"$_ONEC_VERSION"-k8s \
+    docker build -t thedemoncat/onec/full:"$_ONEC_VERSION"-k8s \
         --build-arg ONEC_VERSION="$_ONEC_VERSION" -f Dockerfile_k8s .
     
 }
