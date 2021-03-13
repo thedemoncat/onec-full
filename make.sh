10402d23fd12d826be11213771c9f43d9763f5cf
ZhdanovR <nl.ruslan@yandex.ru>
2021-03-13 22:15:10 +0600

    Принудительная загрузка образа

--- make.sh
+++ make.sh
@@ -5,16 +5,21 @@ if [ -f ".env" ]; then
 fi
 
 buildImages() {
+
+    
+
     _ONEC_VERSION=$1
     docker build -t thedemoncat/onec/full:"$_ONEC_VERSION" \
         --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
         --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
         --build-arg ONEC_VERSION="$_ONEC_VERSION" .
 
-    docker build -t thedemoncat/onec/full:"$_ONEC_VERSION"-k8s \
-        --build-arg ONEC_VERSION="$_ONEC_VERSION" -f Dockerfile_k8s .
+    # docker build -t thedemoncat/onec/full:"$_ONEC_VERSION"-k8s \
+    #     --build-arg ONEC_VERSION="$_ONEC_VERSION" -f Dockerfile_k8s .
     
 }
+
+docker pull docker.pkg.github.com/thedemoncat/onec-base/onec_base:latest
 
 env=()
 while IFS= read -r line || [[ "$line" ]]; do
