ebce6c9ec1c7b75a92fbcea51c9d921633cf59a5
ZhdanovR <nl.ruslan@yandex.ru>
2021-03-15 13:01:33 +0600

    Публикация образа

--- make.sh
+++ make.sh
@@ -6,18 +6,18 @@ fi
 
 buildImages() {
 
-    
-
     _ONEC_VERSION=$1
-    docker build -t thedemoncat/onec/full:"$_ONEC_VERSION" \
+    docker build -t $2:"$_ONEC_VERSION" \
         --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
         --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
         --build-arg ONEC_VERSION="$_ONEC_VERSION" .
 
-    docker build -t thedemoncat/onec/full:"$_ONEC_VERSION"-k8s \
+    docker build -t $2:"$_ONEC_VERSION"-k8s \
         --build-arg ONEC_VERSION="$_ONEC_VERSION" -f Dockerfile_k8s .
     
 }
+
+IMAGE_NAME=${1:-"thedemoncat/onec/full"}
 
 env=()
 while IFS= read -r line || [[ "$line" ]]; do
@@ -27,5 +27,5 @@ done < ONEC_VERSION
 for item in ${env[*]}
 do
     echo "launching an image build with a platform:: "$item
-    buildImages $item
+    buildImages $item $IMAGE_NAME
 done
