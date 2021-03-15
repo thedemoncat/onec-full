461ee527cb25d59f789a5cb683d6175007aa55ee
Ruslan Zhdanov <26283990+TheDemonCat@users.noreply.github.com>
2021-03-15 17:49:33 +0600

    Публикация образа (#4)
    * Сборка образа в реестр

--- make.sh
+++ make.sh
@@ -4,20 +4,7 @@ if [ -f ".env" ]; then
     export $(grep -v '^#' .env | xargs)
 fi
 
-buildImages() {
-
-    
-
-    _ONEC_VERSION=$1
-    docker build -t thedemoncat/onec/full:"$_ONEC_VERSION" \
-        --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
-        --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
-        --build-arg ONEC_VERSION="$_ONEC_VERSION" .
-
-    docker build -t thedemoncat/onec/full:"$_ONEC_VERSION"-k8s \
-        --build-arg ONEC_VERSION="$_ONEC_VERSION" -f Dockerfile_k8s .
-    
-}
+IMAGE_NAME=${1:-"thedemoncat/onec/full"}
 
 env=()
 while IFS= read -r line || [[ "$line" ]]; do
@@ -26,6 +13,12 @@ done < ONEC_VERSION
 
 for item in ${env[*]}
 do
-    echo "launching an image build with a platform:: "$item
-    buildImages $item
+    docker build -t $IMAGE_NAME:"$item" \
+        --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
+        --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
+        --build-arg ONEC_VERSION="$item" .
+
+    docker build -t $IMAGE_NAME:"$item"-k8s \
+        --build-arg IMAGE_NAME="$IMAGE_NAME" \
+        --build-arg ONEC_VERSION="$item" -f Dockerfile_k8s .
 done
