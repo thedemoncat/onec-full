46c1360c0b6acf58ed9b3c1e553070b4ffbe3bc2
Ruslan Zhdanov <26283990+thedemoncat@users.noreply.github.com>
2022-05-22 11:31:27 +0600

    Develop (#9)
    * Реализована поддержка сборки образа 1с с платформой 8.3.20 и выше
    * Из CI убрана сборка образа для платформы ниже 8.3.20
    * Из CI убрана поддержка кеширования, так как не дает пользы из за редкой пересборки образов

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if [ -f ".env" ]; then
     export $(grep -v '^#' .env | xargs)
@@ -11,14 +12,31 @@ while IFS= read -r line || [[ "$line" ]]; do
   env+=("$line")
 done < ONEC_VERSION
 
+
 for item in ${env[*]}
 do
-    docker build -t $IMAGE_NAME:"$item" \
-        --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
-        --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
-        --build-arg ONEC_VERSION="$item" .
+    OLD_VERSION_DIST=19
+    echo "Собираем образ с версией платформы $item"
+    if [ "$(echo $item | cut -d'.' -f3)" -gt "$OLD_VERSION_DIST" ]; then
+        echo "С версии 8.3.20 и выше, дистрибутив установки изенился на run. Собираем по новой схеме сборки"
+        docker build -t $IMAGE_NAME:"$item" \
+            --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
+            --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
+            --build-arg ONEC_VERSION="$item" .
 
-    docker build -t $IMAGE_NAME:"$item"-k8s \
-        --build-arg IMAGE_NAME="$IMAGE_NAME" \
-        --build-arg ONEC_VERSION="$item" -f Dockerfile_k8s .
+        docker build -t $IMAGE_NAME:"$item"-k8s \
+            --build-arg IMAGE_NAME="$IMAGE_NAME" \
+            --build-arg ONEC_VERSION="$item" -f Dockerfile_k8s .
+    else
+        echo "Ниже версии 8.3.20, используется старая версия дистрибутивов (deb пакеты). Собираем по старой схеме сборки"
+        docker build -t $IMAGE_NAME:"$item" \
+            --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
+            --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
+            --build-arg ONEC_VERSION="$item" 
+            -f Dockerfile_deb.
+
+        docker build -t $IMAGE_NAME:"$item"-k8s \
+            --build-arg IMAGE_NAME="$IMAGE_NAME" \
+            --build-arg ONEC_VERSION="$item" -f Dockerfile_k8s .
+    fi 
 done
